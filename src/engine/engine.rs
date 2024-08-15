7329b4f5f7b19033a2160a62863fd9e58966525a
Paul Tsouchlos <developer.paul.123@gmail.com>
2024-08-14 23:15:56 -0400

    wip: start of "real" engine

--- src/engine/engine.rs
+++ src/engine/engine.rs
@@ -1,14 +1,24 @@
-use chess::Board;
+use chess::{Board, ChessMove};
 
-use super::ChessEngine;
+use super::{search, ChessEngine, Timer};
 
 struct ByteKnight {
     /// The current board state
-    board: Board
+    board: Board,
+    search: search::Search
+}
+
+impl ByteKnight {
+    pub fn new() -> ByteKnight {
+        ByteKnight {
+            board: Board::default(),
+            search: search::Search::new()
+        }
+    }
 }
 
 impl ChessEngine for ByteKnight {
-    fn think(&self, board: &Board) -> Option<chess::ChessMove> {
+    fn think(&self, board: &Board, timer: &Timer) -> Option<ChessMove> {
         // TODO
         return None;
     }
