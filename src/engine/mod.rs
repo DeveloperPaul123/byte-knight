8dd370b6aa555e4c13525c9c528b27e63f6bb9fb
Paul Tsouchlos <developer.paul.123@gmail.com>
2024-08-14 23:16:21 -0400

    chore: update module

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,7 +1,11 @@
 mod base_engine;
 mod evil_bot;
 mod engine;
+mod timer;
+mod search;
+mod evaluation;
+mod chess_board_helpers;
 
 pub use base_engine::ChessEngine;
 pub use evil_bot::EvilBot;
-
+pub use timer::Timer;
