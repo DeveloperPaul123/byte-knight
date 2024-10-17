8bcffd2b326bd5cea525305abc9f837de7ac00ae
Paul Tsouchlos <developer.paul.123@gmail.com>
2024-10-16 23:19:06 -0400

    chore: improve `perft`
    Added nps calculation for vanilla `perft` and moved epd processing to its own function

--- src/bin/perft/main.rs
+++ src/bin/perft/main.rs
@@ -44,6 +44,55 @@ where
     Ok(reader.lines().map(|l| l.unwrap()).collect())
 }
 
+fn process_epd_file(path: &str, move_generation: &MoveGenerator) {
+    let mut all_failures = Vec::new();
+    let lines = read_lines(path).unwrap();
+    let now = std::time::Instant::now();
+    lines
+        .par_iter()
+        .map(|line| {
+            let parts: Vec<&str> = line.split(";").collect();
+            let fen = parts[0];
+            let mut failures = Vec::new();
+            for part in parts.iter().skip(1) {
+                let parts = part.split_whitespace().collect::<Vec<&str>>();
+                let depth = parts[0].replace("D", "").parse::<usize>().unwrap();
+                let expected_nodes = parts[1].parse::<u64>().unwrap();
+                let mut board = Board::from_fen(fen).unwrap();
+                let nodes = perft::perft(&mut board, &move_generation, depth, false).unwrap();
+                if expected_nodes != nodes {
+                    println!(
+                        "{:<30}: {:2} {:^10} != {:^10} {}",
+                        fen, depth, expected_nodes, nodes, CROSS_MARK
+                    );
+                    failures.push((fen.to_string(), depth, expected_nodes, nodes));
+                } else {
+                    println!(
+                        "{:<30}: {:2} {:^10} == {:^10} {}",
+                        fen, depth, expected_nodes, nodes, CHECK_BOX
+                    );
+                }
+            }
+
+            failures
+        })
+        .collect_into_vec(&mut all_failures);
+    let elapsed = now.elapsed();
+
+    println!(
+        "Summary:\n\t{} failed\n\t{:.2} seconds",
+        all_failures.iter().map(|f| f.len()).sum::<usize>(),
+        elapsed.as_secs_f64()
+    );
+
+    for (fen, depth, expected, actual) in all_failures.iter().flatten() {
+        println!(
+            "{:<30}: {:2} {:^10} != {:^10} {}",
+            fen, depth, expected, actual, CROSS_MARK
+        );
+    }
+}
+
 static CHECK_BOX: Emoji = Emoji("✅", "");
 static CROSS_MARK: Emoji = Emoji("❌", "");
 
@@ -51,54 +100,9 @@ fn main() {
     let args = Args::parse();
     let mut board = Board::from_fen(&args.fen).unwrap();
     let move_generation = MoveGenerator::new();
-    let mut all_failures = Vec::new();
     if args.epd_file.is_some() {
         let path = args.epd_file.as_ref().unwrap();
-        let lines = read_lines(path).unwrap();
-        let now = std::time::Instant::now();
-        lines
-            .par_iter()
-            .map(|line| {
-                let parts: Vec<&str> = line.split(";").collect();
-                let fen = parts[0];
-                let mut failures = Vec::new();
-                for part in parts.iter().skip(1) {
-                    let parts = part.split_whitespace().collect::<Vec<&str>>();
-                    let depth = parts[0].replace("D", "").parse::<usize>().unwrap();
-                    let expected_nodes = parts[1].parse::<u64>().unwrap();
-                    let mut board = Board::from_fen(fen).unwrap();
-                    let nodes = perft::perft(&mut board, &move_generation, depth, false).unwrap();
-                    if expected_nodes != nodes {
-                        println!(
-                            "{:<30}: {:2} {:^10} != {:^10} {}",
-                            fen, depth, expected_nodes, nodes, CROSS_MARK
-                        );
-                        failures.push((fen.to_string(), depth, expected_nodes, nodes));
-                    } else {
-                        println!(
-                            "{:<30}: {:2} {:^10} == {:^10} {}",
-                            fen, depth, expected_nodes, nodes, CHECK_BOX
-                        );
-                    }
-                }
-
-                failures
-            })
-            .collect_into_vec(&mut all_failures);
-        let elapsed = now.elapsed();
-
-        println!(
-            "Summary:\n\t{} failed\n\t{:.2} seconds",
-            all_failures.iter().map(|f| f.len()).sum::<usize>(),
-            elapsed.as_secs_f64()
-        );
-
-        for (fen, depth, expected, actual) in all_failures.iter().flatten() {
-            println!(
-                "{:<30}: {:2} {:^10} != {:^10} {}",
-                fen, depth, expected, actual, CROSS_MARK
-            );
-        }
+        process_epd_file(path, &move_generation);
     } else if args.split_perft {
         println!("running split perft at depth {}", args.depth);
         let move_results =
@@ -110,8 +114,19 @@ fn main() {
         // print the total nodes
         println!("{}", move_results.iter().map(|r| r.nodes).sum::<u64>());
     } else {
-        let nodes = perft::perft(&mut board, &move_generation, args.depth, false).unwrap();
-        println!("{}", nodes);
+        for i in 1..args.depth + 1 {
+            let now = std::time::Instant::now();
+            let nodes = perft::perft(&mut board, &move_generation, i, false).unwrap();
+            let elapsed = now.elapsed();
+            let nps = nodes as f64 / elapsed.as_secs_f64();
+            println!(
+                "perft {} = {:>12} {:.2} sec {:>12} nps",
+                i,
+                nodes,
+                elapsed.as_secs_f64(),
+                nps.round()
+            );
+        }
     };
 
     // println!("{:?}", result);
