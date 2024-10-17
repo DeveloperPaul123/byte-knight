b9e06cf5841bebf58fd63235e23b8dd9824083c5
Paul Tsouchlos <developer.paul.123@gmail.com>
2024-10-16 23:16:20 -0400

    chore: switch to ArrayVec in `MoveList`

--- board/move_list.rs
+++ board/move_list.rs
@@ -1,38 +1,34 @@
-use crate::{
-    definitions::{MAX_MOVES, MAX_MOVE_LIST_SIZE},
-    moves::Move,
-};
+use arrayvec::ArrayVec;
+
+use crate::{definitions::MAX_MOVE_LIST_SIZE, moves::Move};
 
 pub struct MoveList {
-    size: usize,
-    moves: [Move; MAX_MOVE_LIST_SIZE],
+    moves: ArrayVec<Move, MAX_MOVE_LIST_SIZE>,
 }
 
 impl MoveList {
     pub fn new() -> Self {
         MoveList {
-            size: 0,
-            moves: [Move::default(); MAX_MOVE_LIST_SIZE],
+            moves: ArrayVec::new(),
         }
     }
 
     pub fn len(&self) -> usize {
-        self.size
+        self.moves.len()
     }
 
     pub fn push(&mut self, mv: Move) {
-        if self.size >= MAX_MOVES {
+        let overflow = self.moves.try_push(mv);
+        if overflow.is_err() {
             panic!("MoveList is full");
         }
-        self.moves[self.size] = mv;
-        self.size += 1;
     }
 
     pub fn iter(&self) -> impl Iterator<Item = &Move> {
-        self.moves.iter().take(self.size)
+        self.moves.iter()
     }
 
     pub fn clear(&mut self) {
-        self.size = 0;
+        self.moves.clear();
     }
 }
