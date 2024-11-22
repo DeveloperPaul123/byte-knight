0b8f038b2e99b14837c5add4d36733fdd84755a7
Paul Tsouchlos <developer.paul.123@gmail.com>
2024-11-22 11:57:55 -0500

    chore: update run_cutechess script
    bench: 2577543

--- scripts/run_cutechess.sh
+++ scripts/run_cutechess.sh
@@ -1,1 +1,9 @@
-"F:\Repositories\cutechess\build-qt6\Release\cutechess-cli" -repeat -variant standard -concurrency 10 -games 640 -engine dir=target/release cmd=target/release/byte-knight.exe proto=uci tc=2.19+0.02 timemargin=250 option.Threads=1 option.Hash=16 name=ByteKnight-dev -engine dir=target/release cmd=target/release/byte-knight.exe proto=uci tc=2.19+0.02 timemargin=250 option.Threads=1 option.Hash=16 name=ByteKnight-base -openings file=data/Pohl.epd format=epd order=random start=1 -srand 20 -pgnout 20.28.1731517392.0.pgn -debug
+#!/bin/bash
+
+cutechess-cli -variant standard -concurrency 12 -games 768 \
+    -engine dir=./target/debug cmd="./byte-knight" proto=uci tc=2.27+0.02 timemargin=250 option.Threads=1 option.Hash=16 name=ByteKnight-dev \
+    -engine dir=./target/debug cmd="./byte-knight" proto=uci tc=2.27+0.02 timemargin=250 option.Threads=1 option.Hash=16 name=ByteKnight-base \
+    -openings file="./data/Pohl.epd" format=epd order=random start=1921 \
+    -srand 31 \
+    -pgnout 31.48.1732290528.0.pgn \
+    # -debug
