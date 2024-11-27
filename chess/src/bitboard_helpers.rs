971cdee2abc0815388116774080fcedd6f7185dc
Paul Tsouchlos <developer.paul.123@gmail.com>
2024-11-27 09:20:39 -0500

    fix: auto-formatting
    bench: 2577543

--- chess/src/bitboard_helpers.rs
+++ chess/src/bitboard_helpers.rs
@@ -9,43 +9,43 @@
  * Copyright (c) 2024 Paul Tsouchlos (DeveloperPaul123)
  * GNU General Public License v3.0 or later
  * https://www.gnu.org/licenses/gpl-3.0-standalone.html
- * 
+ *
  */
 
 use crate::bitboard::Bitboard;
 
 /// Returns the index of the next bit set to 1 in the bitboard and sets it to 0.
-/// 
+///
 /// # Arguments
-/// 
+///
 /// * `bitboard` - The bitboard to get the next bit from.
-/// 
+///
 /// # Returns
-/// 
+///
 /// The index of the next bit set to 1 in the bitboard.
-/// 
+///
 /// # Examples
-/// 
+///
 /// ```
 /// use chess::bitboard::Bitboard;
 /// use chess::bitboard_helpers::next_bit;
-/// 
+///
 /// let mut bb = Bitboard::new(0x8000000000000001);
 /// assert_eq!(next_bit(&mut bb), 0);
 /// assert_eq!(next_bit(&mut bb), 63);
 /// assert_eq!(bb.as_number(), 0);
-/// 
+///
 /// ```
 ///  
 /// ```
 /// use chess::bitboard::Bitboard;
 /// use chess::bitboard_helpers::next_bit;
-/// 
+///
 /// let mut bb = Bitboard::new(0xFFFFFFFFFFFFFFFF);
 /// for i in 0..64 {
 ///    assert_eq!(next_bit(&mut bb), i);
 /// }
-/// 
+///
 /// ```
 ///
 pub fn next_bit(bitboard: &mut Bitboard) -> usize {
