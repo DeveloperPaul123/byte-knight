8494800abc298fdac905661b536eea28ff968de5
Paul Tsouchlos <developer.paul.123@gmail.com>
2024-11-29 15:32:24 -0500

    feat: support hash command and try to fix tt
    Tried to fix TT issues and also added a way to support custom commands in UCI mode. For now, only support `hash` command.  TT still seems to be broken though as the new engine loses spectacularly to the engine on `main`
    
    bench: 2153098

--- engine/src/engine.rs
+++ engine/src/engine.rs
@@ -4,7 +4,7 @@
  * Created Date: Friday, November 15th 2024
  * Author: Paul Tsouchlos (DeveloperPaul123) (developer.paul.123@gmail.com)
  * -----
- * Last Modified: Thu Nov 28 2024
+ * Last Modified: Fri Nov 29 2024
  * -----
  * Copyright (c) 2024 Paul Tsouchlos (DeveloperPaul123)
  * GNU General Public License v3.0 or later
@@ -21,13 +21,17 @@ use chess::board::Board;
 use uci_parser::{UciCommand, UciInfo, UciOption, UciResponse};
 
 use crate::{
-    defs::About, input_handler::InputHandler, search::SearchParameters,
-    search_thread::SearchThread, tt_table::TranspositionTable,
+    defs::About,
+    input_handler::{CommandProxy, EngineCommand, InputHandler},
+    search::SearchParameters,
+    search_thread::SearchThread,
+    tt_table::{self, TranspositionTable},
 };
 
 pub struct ByteKnight {
     input_handler: InputHandler,
     search_thread: SearchThread,
+    transposition_table: Arc<Mutex<TranspositionTable<true>>>,
 }
 
 impl ByteKnight {
@@ -35,6 +39,13 @@ impl ByteKnight {
         ByteKnight {
             input_handler: InputHandler::new(),
             search_thread: SearchThread::new(),
+            transposition_table: Default::default(),
+        }
+    }
+
+    fn clear_hash_tables(&mut self) {
+        if let Ok(tt) = self.transposition_table.lock().as_mut() {
+            tt.clear();
         }
     }
 
@@ -50,75 +61,121 @@ impl ByteKnight {
         );
         let stdout: io::Stdout = io::stdout();
         let mut board = Board::default_board();
-        let tt: Arc<Mutex<TranspositionTable>> = Arc::default();
         'engine_loop: while let Ok(command) = &self.input_handler.receiver().recv() {
             let mut stdout = stdout.lock();
+
             match command {
-                UciCommand::Quit => {
-                    // clean up
-                    self.search_thread.exit();
-                    self.input_handler.exit();
-                    break 'engine_loop;
-                }
-                UciCommand::IsReady => {
-                    writeln!(stdout, "{}", UciResponse::<String>::ReadyOk).unwrap();
-                }
-                UciCommand::Uci => {
-                    let id = UciResponse::Id {
-                        name: About::NAME,
-                        author: About::AUTHORS,
-                    };
+                CommandProxy::Uci(uci_command) => match uci_command {
+                    UciCommand::Quit => {
+                        // clean up
+                        self.search_thread.exit();
+                        self.input_handler.exit();
+                        break 'engine_loop;
+                    }
+                    UciCommand::IsReady => {
+                        writeln!(stdout, "{}", UciResponse::<String>::ReadyOk).unwrap();
+                    }
+                    UciCommand::Uci => {
+                        let id = UciResponse::Id {
+                            name: About::NAME,
+                            author: About::AUTHORS,
+                        };
 
-                    let options = vec![
-                        UciOption::spin("Hash", 16, 1, 1024),
-                        UciOption::spin("Threads", 1, 1, 1),
-                    ];
-                    // TODO: Actually implement the hash option
-                    for option in options {
-                        writeln!(stdout, "{}", UciResponse::Option(option)).unwrap();
-                    }
-                    writeln!(stdout, "{}", id).unwrap();
-                    writeln!(stdout, "{}", UciResponse::<String>::UciOk).unwrap();
-                }
-                UciCommand::UciNewGame => {
-                    board = Board::default_board();
-                    if let Ok(tt) = tt.lock().as_mut() {
-                        tt.clear();
-                    }
-                }
-                UciCommand::Position { fen, moves } => {
-                    match fen {
-                        None => {
-                            board = Board::default_board();
+                        let options = vec![
+                            UciOption::spin("Hash", 16, 1, 1024),
+                            UciOption::spin("Threads", 1, 1, 1),
+                        ];
+                        // TODO: Actually implement the hash option
+                        for option in options {
+                            writeln!(stdout, "{}", UciResponse::Option(option)).unwrap();
                         }
-                        Some(fen) => {
-                            board = Board::from_fen(fen.as_str()).unwrap();
-                        }
+                        writeln!(stdout, "{}", id).unwrap();
+                        writeln!(stdout, "{}", UciResponse::<String>::UciOk).unwrap();
                     }
+                    UciCommand::UciNewGame => {
+                        board = Board::default_board();
+                        self.clear_hash_tables();
+                    }
+                    UciCommand::Position { fen, moves } => {
+                        match fen {
+                            None => {
+                                board = Board::default_board();
+                            }
+                            Some(fen) => {
+                                board = Board::from_fen(fen.as_str()).unwrap();
+                            }
+                        }
 
-                    for mv in moves {
-                        board.make_uci_move(&mv.to_string()).unwrap();
+                        for mv in moves {
+                            board.make_uci_move(&mv.to_string()).unwrap();
+                        }
                     }
-                }
-                UciCommand::Go(search_options) => {
-                    if self.search_thread.is_searching() {
-                        eprintln!("Attempting to start a search while already searching");
+                    UciCommand::Go(search_options) => {
+                        if self.search_thread.is_searching() {
+                            eprintln!("Attempting to start a search while already searching");
+                            self.search_thread.stop_search();
+                        }
+
+                        let info =
+                            UciInfo::default().string(format!("searching {}", board.to_fen()));
+                        writeln!(stdout, "{}", UciResponse::info(info)).unwrap();
+
+                        // create the search parameters
+                        let search_params = SearchParameters::new(search_options, &board);
+                        // send them and the current board to the search thread
+                        self.search_thread.start_search(
+                            &board,
+                            search_params,
+                            self.transposition_table.clone(),
+                        );
+                    }
+                    UciCommand::SetOption { name, value } => {
+                        if name.to_lowercase() == "hash" {
+                            if let Some(val) = value {
+                                // set the hash size, making sure it is within the bounds we have set.
+                                if let Ok(hash_size) = val.parse::<usize>() {
+                                    if hash_size < tt_table::MIN_TABLE_SIZE_MB {
+                                        eprintln!(
+                                            "Hash size too small. Must be at least {} MB",
+                                            tt_table::MIN_TABLE_SIZE_MB
+                                        );
+                                        continue;
+                                    } else if hash_size > tt_table::MAX_TABLE_SIZE_MB {
+                                        eprintln!(
+                                            "Hash size too large. Must be at most {} MB",
+                                            tt_table::MAX_TABLE_SIZE_MB
+                                        );
+                                        continue;
+                                    }
+
+                                    self.transposition_table = Arc::new(Mutex::new(
+                                        TranspositionTable::from_size_in_mb(hash_size),
+                                    ));
+                                }
+                            }
+                        }
+                    }
+                    UciCommand::Stop => {
                         self.search_thread.stop_search();
                     }
-
-                    let info = UciInfo::default().string(format!("searching {}", board.to_fen()));
-                    writeln!(stdout, "{}", UciResponse::info(info)).unwrap();
-
-                    // create the search parameters
-                    let search_params = SearchParameters::new(search_options, &board);
-                    // send them and the current board to the search thread
-                    self.search_thread
-                        .start_search(&board, search_params, tt.clone());
-                }
-                UciCommand::Stop => {
-                    self.search_thread.stop_search();
-                }
-                _ => {}
+                    _ => {}
+                },
+                CommandProxy::Engine(engine_command) => match engine_command {
+                    EngineCommand::HashInfo => {
+                        if let Ok(tt) = self.transposition_table.lock() {
+                            writeln!(
+                                stdout,
+                                "full: {:.2}% hits: {} access: {} collisions: {} cap: {}",
+                                tt.fullness(),
+                                tt.hits,
+                                tt.accesses,
+                                tt.collisions,
+                                tt.size(),
+                            )
+                            .unwrap();
+                        }
+                    }
+                },
             }
         }
 
--- engine/src/tt_table.rs
+++ engine/src/tt_table.rs
@@ -4,7 +4,7 @@
  * Created Date: Thursday, November 21st 2024
  * Author: Paul Tsouchlos (DeveloperPaul123) (developer.paul.123@gmail.com)
  * -----
- * Last Modified: Thu Nov 28 2024
+ * Last Modified: Fri Nov 29 2024
  * -----
  * Copyright (c) 2024 Paul Tsouchlos (DeveloperPaul123)
  * GNU General Public License v3.0 or later
@@ -54,22 +54,30 @@ impl TranspositionTableEntry {
 }
 
 /// A transposition table used to store the results of previous searches.
-pub struct TranspositionTable {
+pub struct TranspositionTable<const TRACK_STATS: bool> {
     table: Vec<Option<TranspositionTableEntry>>,
+    pub(crate) collisions: usize,
+    pub(crate) accesses: usize,
+    pub(crate) hits: usize,
 }
 
-const DEFAULT_TABLE_SIZE_MB: usize = 16;
+pub const MAX_TABLE_SIZE_MB: usize = 1024;
+pub const MIN_TABLE_SIZE_MB: usize = 16;
+const DEFAULT_TABLE_SIZE_MB: usize = MIN_TABLE_SIZE_MB;
 
-impl Default for TranspositionTable {
+impl<const TRACK_STATS: bool> Default for TranspositionTable<TRACK_STATS> {
     fn default() -> Self {
         Self::from_size_in_mb(DEFAULT_TABLE_SIZE_MB)
     }
 }
 
-impl TranspositionTable {
+impl<const TRACK_STATS: bool> TranspositionTable<TRACK_STATS> {
     pub(crate) fn from_capacity(capacity: usize) -> Self {
         Self {
             table: vec![None; capacity],
+            collisions: 0,
+            accesses: 0,
+            hits: 0,
         }
     }
 
@@ -82,13 +90,32 @@ impl TranspositionTable {
         zobrist as usize % self.table.len()
     }
 
-    pub(crate) fn get_entry(&self, zobrist: u64) -> Option<TranspositionTableEntry> {
+    pub(crate) fn get_entry(&mut self, zobrist: u64) -> Option<TranspositionTableEntry> {
+        if TRACK_STATS {
+            self.accesses += 1;
+        }
+
         let index = self.get_index(zobrist);
-        self.table[index]
+        let entry = self.table[index];
+
+        if TRACK_STATS {
+            if entry.is_some() {
+                self.hits += 1;
+            }
+        }
+        entry
     }
 
     pub(crate) fn store_entry(&mut self, entry: TranspositionTableEntry) {
         let index = self.get_index(entry.zobrist);
+        if TRACK_STATS {
+            let old = self.table[index];
+            if let Some(old) = old {
+                if old.zobrist == entry.zobrist {
+                    self.collisions += 1;
+                }
+            }
+        }
         self.table[index] = Some(entry);
     }
 
@@ -96,5 +123,19 @@ impl TranspositionTable {
         self.table.iter_mut().for_each(|element| {
             *element = None;
         });
+
+        // reset stats as well
+        self.collisions = 0;
+        self.accesses = 0;
+        self.hits = 0;
+    }
+
+    pub(crate) fn fullness(&self) -> f64 {
+        (self.table.iter().filter(|entry| entry.is_some()).count() as f64 / self.table.len() as f64)
+            * 100_f64
+    }
+
+    pub(crate) fn size(&self) -> usize {
+        self.table.len()
     }
 }
