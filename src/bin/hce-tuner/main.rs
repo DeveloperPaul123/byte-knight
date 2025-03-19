8ab57358d64a24bde790558994086e782c20c1fd
Paul Tsouchlos <developer.paul.123@gmail.com>
2025-03-18 22:20:52 -0400

    feat: add ability to plot k v. error in tuner

--- src/bin/hce-tuner/main.rs
+++ src/bin/hce-tuner/main.rs
@@ -1,6 +1,12 @@
+use std::process::exit;
+
 use chess::{definitions::NumberOf, pieces::PIECE_NAMES};
 use clap::Parser;
+use indicatif::ParallelProgressIterator;
 use parameters::Parameters;
+use rayon::iter::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator};
+use textplots::{Chart, Plot, Shape};
+use tuner::Tuner;
 use tuner_score::TuningScore;
 mod epd_parser;
 mod math;
@@ -17,6 +23,13 @@ struct Options {
     input_data: String,
     #[clap(short, long, help = "Number of epochs to run.")]
     epochs: Option<usize>,
+    #[clap(
+        long,
+        action,
+        default_value_t = false,
+        help = "Plot k versus error for the given dataset"
+    )]
+    plot_k: bool,
 }
 
 fn print_table(indent: usize, table: &[TuningScore]) {
@@ -50,6 +63,26 @@ fn print_params(params: &Parameters) {
     println!("];");
 }
 
+fn plot_k(tuner: &Tuner) {
+    let mut points = Vec::new();
+    let data_point_count = 10_000;
+    let k_min = -0.1;
+    let k_max = 0.1;
+    (0..data_point_count)
+        .into_par_iter()
+        .progress_count(data_point_count as u64)
+        .map(|val| {
+            let k = val as f64 / data_point_count as f64 * (k_max - k_min) + k_min;
+            let error = tuner.mean_square_error(k as f64);
+            (k as f32, error as f32)
+        })
+        .collect_into_vec(&mut points);
+
+    Chart::new(180, 60, k_min as f32, k_max as f32)
+        .lineplot(&Shape::Points(points.as_slice()))
+        .display();
+}
+
 fn main() {
     let options = Options::parse();
     println!("Reading data from: {}", options.input_data);
@@ -60,6 +93,12 @@ fn main() {
     let epochs = options.epochs.unwrap_or(10_000);
     let parameters = Parameters::create_from_engine_values();
     let mut tuner = tuner::Tuner::new(parameters, &positions, epochs);
+
+    if options.plot_k {
+        plot_k(&tuner);
+        exit(0);
+    }
+
     let tuned_result = tuner.tune();
 
     print_params(tuned_result);
