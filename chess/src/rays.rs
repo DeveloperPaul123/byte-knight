ae5b9f43e408323c39a443fcaf0f649f976efadd
Paul Tsouchlos <developer.paul.123@gmail.com>
2026-01-07 18:08:48 -0500

    chore: finish rays initialization code
    bench: 1049220

--- chess/src/rays.rs
+++ chess/src/rays.rs
@@ -17,7 +17,12 @@ const fn initialize_rays_between() -> [[Bitboard; NumberOf::SQUARES]; NumberOf::
                 );
             }
 
-            // TODO(PT): Add bishop rays as well
+            if attacks::bishop(from, Bitboard::default()).intersects(Bitboard::from_square(to)) {
+                rays_between[from as usize][to as usize] = Bitboard::new(
+                    attacks::bishop(from, Bitboard::from_square(to)).as_number()
+                        & attacks::bishop(to, Bitboard::from_square(from)).as_number(),
+                );
+            }
 
             to += 1;
         }
@@ -28,33 +33,39 @@ const fn initialize_rays_between() -> [[Bitboard; NumberOf::SQUARES]; NumberOf::
     rays_between
 }
 
-/// Returns the bitboard representing the ray between two squares.
+/// Returns the [`Bitboard`] representing the ray between two squares.
 ///
+/// # Arguments
+/// - `from`: The starting square (0-63).
+/// - `to`: The ending square (0-63).
+///
+/// # Returns
+/// - A [`Bitboard`] representing the squares between `from` and `to`.
 pub fn between(from: u8, to: u8) -> Bitboard {
     RAYS_BETWEEN[from as usize][to as usize]
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::{move_generation::MoveGenerator, pieces::SQUARE_NAME, square::Square};
+    use crate::{move_generation::MoveGenerator, pieces::SQUARE_NAME, square::Square};
 
-    // #[test]
-    // fn validate_rays_between() {
-    //     let move_gen = MoveGenerator::new();
-    //     for from in 0..64_u8 {
-    //         for to in 0..64_u8 {
-    //             let bb = super::between(from, to);
-    //             let move_gen_bb = move_gen.ray_between(
-    //                 Square::from_square_index(from),
-    //                 Square::from_square_index(to),
-    //             );
-    //             println!(
-    //                 "{} -> {}\n{}\n{}",
-    //                 SQUARE_NAME[from as usize], SQUARE_NAME[to as usize], bb, move_gen_bb
-    //             );
+    #[test]
+    fn validate_rays_between() {
+        let move_gen = MoveGenerator::new();
+        for from in 0..64_u8 {
+            for to in 0..64_u8 {
+                let bb = super::between(from, to);
+                let move_gen_bb = move_gen.ray_between(
+                    Square::from_square_index(from),
+                    Square::from_square_index(to),
+                );
+                println!(
+                    "{} -> {}\n{}\n{}",
+                    SQUARE_NAME[from as usize], SQUARE_NAME[to as usize], bb, move_gen_bb
+                );
 
-    //             assert_eq!(bb, move_gen_bb);
-    //         }
-    //     }
-    // }
+                assert_eq!(bb, move_gen_bb);
+            }
+        }
+    }
 }
